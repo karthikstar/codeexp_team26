ccc3c4cc22b0d2541c30ef0bb7bd145ed704b025
karthikstar <elkarthi2000@gmail.com>
2022-06-12 14:46:19 +0800

    added new screen components

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './Screens/HomeScreen';
+import CalenderScreen from './Screens/CalenderScreen';
+import DiscountsScreen from './Screens/DiscountsScreen';
+import DistanceTrackerScreen from './Screens/DistanceTrackerScreen';
+const Tab = createBottomTabNavigator();
+import { FontAwesome } from "@expo/vector-icons";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            // Set the Icon based on which route it is (name of the tab)
+            if (route.name === 'Home'){
+              iconName = "home";
+            } else if (route.name === "Calender"){
+              iconName = "list";
+            } else if (route.name === "Discounts"){
+              iconName = "list";
+            } else if (route.name === "DistanceTracker"){
+              iconName = "list";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'hotpink',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Calender" component={CalenderScreen} />
+        <Tab.Screen name="Discounts" component={DiscountsScreen} />
+        <Tab.Screen name="DistanceTracker" component={DistanceTrackerScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screens/DistanceTrackerScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function DistanceTrackerScreen () {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Distance tracking for NS FIT</Text>
+      </View>
+    );
+  }

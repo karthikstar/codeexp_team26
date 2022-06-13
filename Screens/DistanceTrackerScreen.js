baced0680b5dc3f7ed852347f4b96f79b4b90ad6
karthikstar <elkarthi2000@gmail.com>
2022-06-13 23:36:01 +0800

    distance comp v1 not working well

--- Screens/DistanceTrackerScreen.js
+++ Screens/DistanceTrackerScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Distance from '../components/Distance';
 
 export default function DistanceTrackerScreen () {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Distance />
         <Text>Distance tracking for NS FIT</Text>
       </View>
     );
